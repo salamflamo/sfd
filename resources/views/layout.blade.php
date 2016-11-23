1712824f88eb9f0aa9d15b64960d819d00c7561c
wkwksama <mailpakgasing@gmail.com>
2016-11-23 21:32:47 +0700

    ngasih layout blade

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Daftar exhibition</title>
+    <!--Import Google Icon Font-->
+    <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!--Import materialize.css-->
+    <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+    <!--Let browser know website is optimized for mobile-->
+    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    <!--css ku dewe-->
+      <style type="text/css">
+          img.logosfd{
+              width: 300px;
+              margin: 10px;
+          }
+          span.wajib{
+              color: red;
+          }
+          .warnaku{
+            background-color: #202440 !important;
+          }
+          img.homelogo{
+              width: 50px;
+              height: auto;
+              margin: 0 0 0 20px;
+              padding: 0;
+          }
+          .brand-logo:hover{
+              background-color: transparent !important;
+          }
+      </style>
+</head>
+<body>
+
+  @yield('content')
+
+  <script type="text/javascript" src="https://code.jquery.com/jquery-2.1.1.min.js"></script>
+  <script type="text/javascript" src="js/materialize.min.js"></script>
+</body>
+</html>
