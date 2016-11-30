da2acbbb85f030b64364955bfdfb26f5c24f3de6
salamflamo <untungsuprapto69@gmail.com>
2016-11-30 21:13:01 +0700

    ganti warna

--- resources/views/thanks.blade.php
+++ resources/views/thanks.blade.php
@@ -37,7 +37,7 @@ tapi tidak masuk content -->
     <div class="row">
         <div class="col-lg-12 text-center">
             <h1>Terima Kasih Sudah Mendaftar</h1><br>
-            <h3>Silahkan simpan kode ini <span style="background-color:#ef4b52ff;color: white;padding: 3px 3px;"><?php echo generateRandomString(5); ?></span></h3>
+            <h3>Silahkan simpan kode ini <span style="background-color:#ef4b52;color: white;padding: 3px 3px;"><?php echo generateRandomString(5); ?></span></h3>
         </div>
         <div class="col-lg-12 text-center">
             <a href="workshop.blade.php" class="btn btn-xl">Daftarkan yang lain</a>
