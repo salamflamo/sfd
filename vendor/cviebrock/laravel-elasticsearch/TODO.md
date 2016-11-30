9c885a20c456c387eedf0cdcce7f4e65422f4983
arnaz adiputra <arnazadiputra@gmail.com>
2016-11-30 22:52:22 +0700

    hapus vendor

--- vendor/cviebrock/laravel-elasticsearch/TODO.md
+++
@@ -1,5 +0,0 @@
-# To-do List
-
-- [ ] More documentation
-- [ ] Allow for client "wrappers" that add functionality to the client (e.g., automatic index prefixing/suffixing, index templates configuration, etc.) 
-- [ ] Tests!
--- vendor/magyarjeti/loripsum-client/doc/elementlist.js
+++
@@ -1,3 +0,0 @@
-
-var ApiGen = ApiGen || {};
-ApiGen.elements = [["c","Exception"],["c","Magyarjeti\\Loripsum\\Client"],["c","Magyarjeti\\Loripsum\\Http\\AdapterInterface"],["c","Magyarjeti\\Loripsum\\Http\\CurlAdapter"],["c","RuntimeException"]];
